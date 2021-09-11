c18f32295cc391142f5525a7d2c1ce53e5fe8640
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-09-10 22:16:19 -0400

    added typedefs and user resolvers updated auth file.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,43 @@
-const { Tech, Matchup } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
+  Query: {
+      user: async (parent, {user=null, params}) => {
+          return User.findOne({$or: [{ _id: user ? user._id : params.id }, { username: params.username }]}).populate('savedBooks')
+      },
+      me: async (parent, args, context) => {
+        if (context.user) {
+          return User.findOne({ _id: context.user._id }).populate('savedBooks');
+        }
+        throw new AuthenticationError('You need to be logged in!');
+      },
+    },
+  Mutation: {
+    addUser: async (parent, { body }) => {
+        const user = await User.create(body);
+        const token = signToken(user);
+        return { token, user };
+      },  
+    login: async (parent, { body }) => {
+        const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
 
+        if (!user) {
+            throw new AuthenticationError("Can't find this user!");
+        }
+
+        const correctPw = await user.isCorrectPassword(body.password);
+
+        if (!correctPw) {
+            throw new AuthenticationError('Incorrect credentials');
+        }
+
+        const token = signToken(user);
+
+        return { token, user };
+      },
+  }
 };
 
 module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express')
-const routes = require('./routes');
+// const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
